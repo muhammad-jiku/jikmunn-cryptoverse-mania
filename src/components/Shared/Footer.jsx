8d5038be6654e02c7e6e91368cf0510084141924
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-13 19:57:21 +0600

    Footer

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,10 +1,25 @@
+import { Space, Typography } from 'antd';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <>
+      <Typography.Title
+        level={5}
+        style={{ color: 'white', textAlign: 'center' }}
+      >
+        Copyright © {year}
+        <Link to="/"> Jikmunn Cryptoverse Mania Inc.</Link> <br />
+        All Rights Reserved.
+      </Typography.Title>
+      <Space>
+        <Link to="/">Home</Link>
+        <Link to="/exchanges">Exchanges</Link>
+        <Link to="/news">News</Link>
+      </Space>
+    </>
   );
 };
 
