a76d0bc05ebe52e24b8e1fdab5a6d6840013c8b7
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-13 18:31:27 +0600

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import CryptoDetails from './components/Crypto/Index';
+import CryptoCurrencies from './components/CryptoCurrencies/Index';
+import Exchanges from './components/Exchanges/Index';
+import Homepage from './components/Home/Index';
+import News from './components/News/Index';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there!</h1>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/cryptocurrencies" element={<CryptoCurrencies />} />
+        <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
     </div>
   );
 }
